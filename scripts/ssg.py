130239abfb83135fef5a1f43a1b356ddf22fd3d7
Huy Nguyen <huy55465@gmail.com>
2026-07-30 15:43:48 -0400

    ssg: format after writing
    Signed-off-by: Huy Nguyen <huy55465@gmail.com>

--- scripts/ssg.py
+++ scripts/ssg.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from subprocess import Popen
 
 def process_file(f, vars):
     variables = dict([x for x in vars.items()])
@@ -93,5 +94,8 @@ if __name__ == "__main__":
         path = Path(sys.argv[2])
         path.parent.mkdir(parents=True, exist_ok=True)
         path.write_text(process_file(f, vars), encoding="utf-8")
-        print(f"Data written to {sys.argv[2]}")
+        print(f"Data written to {sys.argv[2]}\nFormatting...")
+        # format
+        Popen(["prettier", "-w", sys.argv[2]], stderr=sys.stderr).communicate()
+            
         # print(process_file(f, vars))
