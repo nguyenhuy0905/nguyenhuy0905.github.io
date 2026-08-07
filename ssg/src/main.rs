711d3a4e8f7301ddcbf7372735bc57ef180f556d
Huy Nguyen <huy55465@gmail.com>
2026-08-06 23:20:04 -0400

    ssg: change main() exe.run example for no reason
    Signed-off-by: Huy Nguyen <huy55465@gmail.com>

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -7,7 +7,7 @@ use exe::Runner;
 fn main() {
     let mut exe = Runner::new();
     let mut out: Vec<u8> = Vec::new();
-    exe.run("<h1>{{ yield hello; }}</h1>".as_bytes(), &mut out);
+    exe.run("<h1>{{ hello = 1;\n yield hello; }}</h1>".as_bytes(), &mut out);
     println!("Output:\n{}", String::from_utf8(out).unwrap());
     // println!("Hello, world!");
 }
